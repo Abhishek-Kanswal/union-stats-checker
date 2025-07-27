a89f32ec14ad1d88a7b79169f68e03919cda964b
abhishektopisop <sarojkanswal@gmail.com>
2025-07-27 15:59:36 +0530

    Add new UI feature

--- src/components/ui/YapperSearchBar.jsx
+++ src/components/ui/YapperSearchBar.jsx
@@ -19,22 +19,23 @@ const YapperSearchBar = ({ onSearch }) => {
   };
 
   return (
-    <div className="relative w-full mx-6 mt-4 mx-auto">
+    <div className="relative w-full max-w-xl mx-auto mt-6">
       <input
         type="text"
         placeholder="Search yappers..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
         onKeyDown={handleKeyDown}
-        className="w-full pl-4 pr-10 py-2 rounded-md 
-                   bg-white/10 text-white placeholder-white/60 
-                   border border-white/20 focus:outline-none focus:ring-2 focus:ring-white/30"
+        className="w-full pl-5 pr-12 py-3 text-base rounded-xl 
+                   bg-[#101013] text-white placeholder-white/60 
+                   border border-[#27272A] focus:outline-none 
+                   focus:ring-2 focus:ring-[#27272A] transition-all duration-200"
       />
       <button
         onClick={handleSearch}
-        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white hover:text-white"
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white hover:text-white"
       >
-        <FontAwesomeIcon icon={faSearch} className="text-white/70" />
+        <FontAwesomeIcon icon={faSearch} className="text-white/70 text-lg" />
       </button>
     </div>
   );
